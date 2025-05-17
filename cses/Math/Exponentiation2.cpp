281b3f01b7e3f4427d495b5d026f09cfc0c8fdda
Sebastian Dionicio <sdionicio@dal.ca>
2025-05-16 21:29:59 -0700

    solved exp1 and exp2

--- cses/Math/Exponentiation2.cpp
+++ cses/Math/Exponentiation2.cpp
@@ -2,30 +2,31 @@
 #define ios() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
 #define deb(x) cerr << #x << ": " << x << '\n'
 #define rep(i,n) for(int i = 0; i<n; ++i)
+#define int long long
 using namespace std;
 typedef long long ll;
 typedef vector<int> vi;
 const int MOD = 1e9 + 7;
 
-int pw(int a, int b){
+int pw(int a, int b, int m){
   if(b == 0) return 1;
-  int t = pw(a,b/2) %MOD; t = (1LL * t * t)%MOD;
+  int t = pw(a,b/2,m) %m; t = (1LL * t * t)%m;
   if(b&1){
-    t = (1LL * t * a) % MOD;
+    t = (1LL * t * a) % m;
   }
-  return t;
-  
-    
+  return t;   
 }
 
 void solve(){
-
-  int a, b; cin >> a >> b;
-  cout << pw(a,b) << '\n'; 
+  int a, b, c; cin >> a >> b >> c;
+  int r;
+  if(b==0 && c == 0) r = 1;
+  else r = pw(b,c,MOD-1);
+  cout << pw(a,r,MOD) << '\n';
 }
 
 
-int main(){
+signed main(){
   ios();
   int T = 1;
   cin >> T;
